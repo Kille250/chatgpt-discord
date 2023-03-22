fd43da9dec36383938841f728fdda186cc7273c5
Dionyssios Stamatis <dionyssios.stamatis@perfact.de>
2023-03-22 22:35:43 +0100

    Change it to prod settings

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 intents = discord.Intents.all()
 
-bot = commands.Bot(command_prefix="?", intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 
 async def setup(bot):
